cd9d89888dbc14c2404e137ac2e9d07a5118c309
Michael Grienauer <mgrienauer@gmail.com>
2020-02-17 19:24:10 -0800

    Attempt to edit cms fields

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,63 +1,84 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+	const PageContent = contentComponent || Content;
 
-  return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className="section section--gradient">
+			{/* <div
+				className="full-width-image-container margin-top-0"
+				style={{
+					backgroundImage: `url(${
+						!!image.childImageSharp ? image.childImageSharp.fluid.src : image
+					})`,
+				}}
+			>
+				<h2
+					className="has-text-weight-bold is-size-1"
+					style={{
+						boxShadow: "0.5rem 0 0 #75b79e, -0.5rem 0 0 #75b79e",
+						backgroundColor: "#75b79e",
+						color: "white",
+						padding: "1rem",
+					}}
+				>
+					{title}
+				</h2>
+			</div> */}
+
+			<div className="container">
+				<div className="columns">
+					<div className="column is-10 is-offset-1">
+						<div className="section">
+							<h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+								{title}
+							</h2>
+							<PageContent className="content" content={content} />
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
 AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
+	title: PropTypes.string.isRequired,
+	content: PropTypes.string,
+	contentComponent: PropTypes.func,
+};
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+	const { markdownRemark: post } = data;
 
-  return (
-    <Layout>
-      <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<AboutPageTemplate
+				contentComponent={HTMLContent}
+				title={post.frontmatter.title}
+				content={post.html}
+			/>
+		</Layout>
+	);
+};
 
 AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+	data: PropTypes.object.isRequired,
+};
 
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
-  query AboutPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-      }
-    }
-  }
-`
+	query AboutPage($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			html
+			frontmatter {
+				title
+			}
+		}
+	}
+`;
