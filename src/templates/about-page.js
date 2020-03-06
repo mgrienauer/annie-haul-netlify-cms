3f91a64467abcbb25603683bd72659fe55370e3c
Michael Grienauer <mgrienauer@gmail.com>
2020-03-05 18:48:03 -0800

    About page error hotfix

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -4,37 +4,11 @@ import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 
-export const AboutPageTemplate = ({
-	title,
-	content,
-	contentComponent,
-	image,
-}) => {
+export const AboutPageTemplate = ({ title, content, contentComponent }) => {
 	const PageContent = contentComponent || Content;
 
 	return (
 		<section className="section section--gradient">
-			<div
-				className="full-width-image-container margin-top-0"
-				style={{
-					backgroundImage: `url(${
-						!!image.childImageSharp ? image.childImageSharp.fluid.src : ""
-					})`,
-				}}
-			>
-				<h2
-					className="has-text-weight-bold is-size-1"
-					style={{
-						boxShadow: "0.5rem 0 0 #75b79e, -0.5rem 0 0 #75b79e",
-						backgroundColor: "#75b79e",
-						color: "white",
-						padding: "1rem",
-					}}
-				>
-					{title}
-				</h2>
-			</div>
-
 			<div className="container">
 				<div className="columns">
 					<div className="column is-10 is-offset-1">
