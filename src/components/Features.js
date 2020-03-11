ab77c428d4045995206d9191e8fe3d0143b66112
Michael Grienauer <mgrienauer@gmail.com>
2020-03-11 14:57:18 -0700

    change button styling

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,37 +3,35 @@ import PropTypes from "prop-types";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 const FeatureGrid = ({ gridItems }) => (
-	<section class="section">
-		<div className="columns is-multiline">
-			<div className="column is-12">
-				<h3 className="title has-text-green has-text-weight-semibold is-size-2">
-					{gridItems.heading}
-				</h3>
-				<h4 className="title has-text-green has-text-weight-semibold is-size-3">
-					{gridItems.subheading}
-				</h4>
-			</div>
-
-			{gridItems.blurbs.map(item => (
-				<div key={item.text} className="column is-6">
-					<div className="content">
-						<div className="has-text-centered">
-							<div
-								style={{
-									width: "90%",
-									display: "inline-block",
-									margin: "1em",
-								}}
-							>
-								<PreviewCompatibleImage imageInfo={item} />
-							</div>
-						</div>
-						<p>{item.text}</p>
-					</div>
-				</div>
-			))}
+	<div className="columns is-multiline">
+		<div className="column is-12">
+			<h3 className="title has-text-green has-text-weight-semibold is-size-2">
+				{gridItems.heading}
+			</h3>
+			<h4 className="title has-text-green has-text-weight-semibold is-size-3">
+				{gridItems.subheading}
+			</h4>
 		</div>
-	</section>
+
+		{gridItems.blurbs.map(item => (
+			<div key={item.text} className="column is-6">
+				<div className="content">
+					<div className="has-text-centered">
+						<div
+							style={{
+								width: "90%",
+								display: "inline-block",
+								margin: "1em",
+							}}
+						>
+							<PreviewCompatibleImage imageInfo={item} />
+						</div>
+					</div>
+					<p>{item.text}</p>
+				</div>
+			</div>
+		))}
+	</div>
 );
 
 FeatureGrid.propTypes = {
